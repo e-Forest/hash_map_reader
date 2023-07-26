8c58bac7476d8dc39dcb38b0609fa9290a869f93
e-Forest <50156472+e-Forest@users.noreply.github.com>
2023-07-26 12:05:06 +0200

    fix ret

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,17 @@ use std::fs;
 use std::path::Path;
 
 pub fn from_file(path: &str) -> Result<HashMap<String, String>, Box<dyn Error>> {
-    let ret: HashMap<String, String> = HashMap::new();
+    let mut ret: HashMap<String, String> = HashMap::new();
     let path = Path::new(path);
     let binding = fs::read_to_string(path)?;
     let lines = binding.lines();
-    let mut value = HashMap::new();
     //-- collect hashmap from file
     for line in lines {
         match line.split_once(':') {
             Some((key, val)) => {
                 let key = key.trim().to_string();
                 let val = val.trim().to_string();
-                value.insert(key, val);
+                ret.insert(key, val);
             }
             _ => return Err("line has no delimiter ':'".into()),
         };
