70586748c6e9062fbfbdc63699d313b726cb9603
e-Forest <50156472+e-Forest@users.noreply.github.com>
2023-07-26 11:52:11 +0200

    prepare for lib-use

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs;
+use std::path::Path;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn from_file(path: &str) -> Result<HashMap<String, String>, Box<dyn Error>> {
+    let ret: HashMap<String, String> = HashMap::new();
+    let path = Path::new(path);
+    let binding = fs::read_to_string(path)?;
+    let lines = binding.lines();
+    let mut value = HashMap::new();
+    //-- collect hashmap from file
+    for line in lines {
+        match line.split_once(':') {
+            Some((key, val)) => {
+                let key = key.trim().to_string();
+                let val = val.trim().to_string();
+                value.insert(key, val);
+            }
+            _ => return Err("line has no delimiter ':'".into()),
+        };
     }
+    Ok(ret)
 }
